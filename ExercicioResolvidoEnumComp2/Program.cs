2a209ac35dfc2fb31e498f55470d4ec7f57fe2e1
Angelo Carvalho <angelosuporteti@gmail.com>
2020-07-07 16:26:31 -0300

    Adicionado os Comentarios a Classe Program

--- ExercicioResolvidoEnumComp2/Program.cs
+++ ExercicioResolvidoEnumComp2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ExercicioResolvidoEnumComp2.Entidades;
 
 namespace ExercicioResolvidoEnumComp2
 {
@@ -6,7 +7,22 @@ namespace ExercicioResolvidoEnumComp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Comentario c1 = new Comentario("Tenha uma boa viagem!");
+            Comentario c2 = new Comentario("Legal");
+
+            Post p1 = new Post(DateTime.Parse("01/01/2020 16:01:44"), "Viajando", "Indo passear um pouco", 10);
+            p1.AddComentario(c1);
+            p1.AddComentario(c2);
+
+            Comentario c3 = new Comentario("Boa noite");
+            Comentario c4 = new Comentario("Quando retornar conte como foi a viagem");
+
+            Post p2 = new Post(DateTime.Parse("02/01/2020 20:01:44"), "Boa noite", "Depois falamos", 8);
+            p2.AddComentario(c3);
+            p2.AddComentario(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
         }
     }
 }
